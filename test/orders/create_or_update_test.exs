779d4f67685c2ca61f33a4b912d089f0ba3a2860
Jefferson Farias <jefferson.farias7@gmail.com>
2021-12-22 08:21:25 -0300

    test for create_or_update orders

--- test/orders/create_or_update_test.exs
+++ test/orders/create_or_update_test.exs
@@ -55,7 +55,11 @@ defmodule Exlivery.Orders.CreateOrUpdateTest do
       assert response == expected_response
     end
 
-    test "when there are invalid items, returns an error", %{user_cpf: cpf, item1: item1, item2: item2} do
+    test "when there are invalid items, returns an error", %{
+      user_cpf: cpf,
+      item1: item1,
+      item2: item2
+    } do
       params = %{user_cpf: cpf, items: [%{item1 | quantity: 0}, item2]}
 
       response = CreateOrUpdate.call(params)
@@ -70,7 +74,7 @@ defmodule Exlivery.Orders.CreateOrUpdateTest do
 
       response = CreateOrUpdate.call(params)
 
-      expected_response = {:error, "Invalid items"}
+      expected_response = {:error, "Invalid parameters"}
 
       assert response == expected_response
     end
